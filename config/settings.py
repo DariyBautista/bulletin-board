c030b365c155061ad1c975fed79bdf54be3245e9
Bautista Dariy <79574522+DariyBautista@users.noreply.github.com>
2025-05-15 19:19:59 +0300

    mail

--- config/settings.py
+++ config/settings.py
@@ -168,3 +168,11 @@ SOCIALACCOUNT_PROVIDERS = {
         'OAUTH_PKCE_ENABLED': True,
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sevenadrews@gmail.com'
+EMAIL_HOST_PASSWORD = 'mldb liha aveu ppel'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
